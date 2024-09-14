f879a5eefa1fd17360ce10d873aced754cec3e6d
Noah <noah.devrz@gmail.com>
2024-09-14 19:07:08 +0200

    added initial commit

---
+++ src/main/java/com/noahrzdemocompany/first_demo_project/App.java
@@ -0,0 +1,10 @@
+package com.noahrzdemocompany.first_demo_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
